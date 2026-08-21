3a0bcd7c561ca8ea13d4fec2e5e2a54b48dbe39b
rudneysilva048-lgtm <rudneysilva048@gmail.com>
2026-08-21 13:18:03 -0300

    Update render_etiqueta_v1.py

--- render_etiqueta_v1.py
+++ render_etiqueta_v1.py
@@ -89,10 +89,10 @@ def render(data, output):
     source = Image.open(BASE).convert("RGB")
 
     crop_box = (
-        0,
-        546,
-        738,
-        1053
+        2,
+        556,
+        736,
+        1046
     )
 
     img = source.crop(crop_box)
