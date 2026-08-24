1696d3599e591e18d08aafc04df5d71f5b937213
rudneysilva048-lgtm <rudneysilva048@gmail.com>
2026-08-24 12:00:00 -0300

    Update render_etiqueta_v1.py

--- render_etiqueta_v1.py
+++ render_etiqueta_v1.py
@@ -113,9 +113,9 @@ def render(data, output):
     
     img = img.filter(
     ImageFilter.UnsharpMask(
-        radius=1.2,
-        percent=160,
-        threshold=2
+        radius=1.5,
+        percent=220,
+        threshold=1
     )
 )
     draw = ImageDraw.Draw(img)
