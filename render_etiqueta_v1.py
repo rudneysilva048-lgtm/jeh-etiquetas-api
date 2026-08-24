03e0695849907ef2ebff842bb0524b749711eb19
rudneysilva048-lgtm <rudneysilva048@gmail.com>
2026-08-24 14:56:25 -0300

    Update render_etiqueta_v1.py

--- render_etiqueta_v1.py
+++ render_etiqueta_v1.py
@@ -89,9 +89,9 @@ def render(data, output):
     source = Image.open(BASE).convert("RGB")
 
     crop_box = (
-        226,
+        176,
         2204,
-        2726,
+        2776,
         4204
     )
 
@@ -113,9 +113,9 @@ def render(data, output):
     
     img = img.filter(
     ImageFilter.UnsharpMask(
-        radius=1.5,
-        percent=700,
-        threshold=0
+        radius=1.2,
+        percent=600,
+        threshold=2
     )
 )
     
